f059924949f150b6ec20e074cb4ce722766ae99e
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2024-10-08 17:44:15 +0200

    Run black and mypy

--- function_app.py
+++ function_app.py
@@ -1,27 +1,39 @@
+import json
 import azure.functions as func
 import dataclasses
+import ijson
 import logging
-from datetime import datetime 
+from datetime import datetime
 
 app = func.FunctionApp()
 
-@app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="cspazure",
-                               connection="cspazure_logsexport_EVENTHUB") 
+
+@app.event_hub_message_trigger(
+    arg_name="azeventhub",
+    event_hub_name="cspazure",
+    connection="cspazure_logsexport_EVENTHUB",
+)
 def logexport(azeventhub: func.EventHubEvent):
-    logging.info('Python EventHub trigger processed an event: %s',
-                azeventhub.get_body().decode('utf-8'))
+    messages = list(records(azeventhub.get_body()))
+    logging.info("Python EventHub trigger processed an event: %s", messages)
 
 
 @dataclasses.dataclass
 class Message:
     body: str
     attributes: dict
-    timestamp: datetime# TODO: support nanoseconds 
+    timestamp: datetime  # TODO: support nanoseconds
 
     @staticmethod
-    def from_json(json: dict):
+    def from_json(load: dict):
         return Message(
-            body=json["body"],
-            attributes=json["attributes"],
-            timestamp=datetime.fromisoformat(json["timestamp"])
+            body=json.dumps(load),  # TODO: decide what the body should be
+            attributes={k: v for k, v in load["properties"].items()},
+            timestamp=datetime.fromisoformat(load["time"]),
         )
+
+
+def records(f):
+    for i in ijson.items(f, "records.item"):
+        # TODO: catch errors
+        yield Message.from_json(i)
--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,13 +1,21 @@
+import json
+from function_app import Message, records
 
-from function_app import Message
 
-def test_deserialization():
-    json = {
-        "body": "test",
-        "attributes": {"key": "value"},
-        "timestamp": "2024-06-05T10:47:31.676Z"
+def test_deserialization_message():
+    load = {"properties": {"key": "value"}, "time": "2024-06-05T10:47:31.676Z"}
+    message = Message.from_json(load)
+    assert json.loads(message.body) == {
+        "time": "2024-06-05T10:47:31.676Z",
+        "properties": {"key": "value"},
     }
-    message = Message.from_json(json)
-    assert message.body == "test"
     assert message.attributes == {"key": "value"}
     assert message.timestamp.day == 5
+
+
+def test_deserialization_records():
+    messages = list()
+    with open("tests/record_sample.json", "rb") as f:
+        messages = list(records(f))
+
+    assert len(messages) == 2
