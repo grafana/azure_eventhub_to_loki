6d6eb1c63c7140af3db9b7f3bff4bd9b3346c40f
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2024-10-08 13:36:11 +0200

    Migrate to Python

---
+++ function_app.py
@@ -0,0 +1,27 @@
+import azure.functions as func
+import dataclasses
+import logging
+from datetime import time 
+
+app = func.FunctionApp()
+
+@app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="cspazure",
+                               connection="cspazure_logsexport_EVENTHUB") 
+def logexport(azeventhub: func.EventHubEvent):
+    logging.info('Python EventHub trigger processed an event: %s',
+                azeventhub.get_body().decode('utf-8'))
+
+
+@dataclasses.dataclass
+class Message:
+    body: str
+    attributes: dict
+    timestamp: time # TODO: support nanoseconds 
+
+    @staticmethod
+    def from_json(json: dict) -> Message:
+        return Message(
+            body=json["body"],
+            attributes=json["attributes"],
+            timestamp=time.fromisoformat(json["timestamp"])
+        )
