80183b0ef39f992e4cdf7dbe6e4d2face4d489be
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2024-12-13 15:22:13 +0100

    Deal with , in label value or name

--- logexport/config.py
+++ logexport/config.py
@@ -7,13 +7,16 @@ ADDITIONAL_LABEL_PREFIX: Final[str] = "ADDITIONAL_LABEL_"
 def get_additional_labels() -> dict[str, str]:
     """Returns a dictionary of additional labels to add to the exported streams.
     The labels are configured through environment variables."""
-    return get_additional_labels_from_env(os.environ)
+    return get_additional_labels_from_mapping(os.environ)
 
 
-def get_additional_labels_from_env(env: Mapping[str, str]) -> dict[str, str]:
+def get_additional_labels_from_mapping(env: Mapping[str, str]) -> dict[str, str]:
     labels = {}
     for key, value in env.items():
         if key.startswith(ADDITIONAL_LABEL_PREFIX):
-            labels[key[len(ADDITIONAL_LABEL_PREFIX) :]] = value
+            label_name = key[len(ADDITIONAL_LABEL_PREFIX) :]
+            if label_name:
+                label_name = label_name.replace(",", "_")
+                labels[label_name] = value.replace(",", "_")
 
     return labels
