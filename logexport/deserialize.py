df916165d578466a3adf1fe0a1b0e28e441846c6
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2026-03-20 14:31:00 +0100

    Use no defualt

--- logexport/deserialize.py
+++ logexport/deserialize.py
@@ -14,7 +14,7 @@ VERSION_LABEL_KEY: Final[str] = "__grafana_azure_logexport_version__"
 
 def entry_from_event_record(
     load: dict, current_ts_nanos: int
-) -> Tuple[str | None, str, str | None, push_pb2.EntryAdapter]:
+) -> Tuple[str | None, str | None, str | None, push_pb2.EntryAdapter]:
     """Returns the category and type of the event and the entry."""
     entry = push_pb2.EntryAdapter()
 
@@ -32,17 +32,13 @@ def entry_from_event_record(
     if "correlationId" in load:
         entry.structuredMetadata.add(name="correlationId", value=load["correlationId"])
 
-    resource = load.get("ResourceGroup")
-    if resource is None:
-        resource = "undefined"
-
     entry.line = json.dumps(load)
 
     typ = load.get("type")
     if typ is None and load.get("ProductName") == "Microsoft Defender for Cloud":
         typ = "Alert/" + (load.get("AlertType") or "Unknown")
 
-    return load.get("category"), resource, typ, entry
+    return load.get("category"), load.get("ResourceGroup"), typ, entry
 
 
 def stream_from_event_body(
@@ -107,7 +103,7 @@ def get_timestamp(load: dict) -> str | None:
 
 def create_labels_string(
     category: str | None,
-    resource: str,
+    resource: str | None,
     type: str | None,
     addional_labels: dict[str, str],
 ) -> str:
@@ -119,7 +115,8 @@ def create_labels_string(
     if category is not None:
         labels += f',category="{category}"'
 
-    labels += f',resource="{resource}"'
+    if resource is not None:
+        labels += f',resource="{resource}"'
 
     if type is not None:
         labels += f',type="{type}"'
